c2eb025889c1604672a18380f939c99f45c140ae
jorsilver <jorsilver@chapman.edu>
2024-05-02 12:51:05 -0700

    fixed conn syntax

--- Hello.py
+++ Hello.py
@@ -16,7 +16,17 @@ import streamlit as st
 from streamlit.logger import get_logger
 import mysql.connector
 
-LOGGER = get_logger(__name__)
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="CPSC408!",
+    auth_plugin='mysql_native_password',
+    database ="Fitness")
+    
+cursor = connection.cursor()
+
+print("Connection made..\n")
+
 
 
 def run():
@@ -24,39 +34,6 @@ def run():
         page_title="Hello",
         page_icon="👋",
     )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-    connection = mysql.connector.connect(
-            host="127.0.0.1",
-            user="root",
-            password="CPSC408!",
-            auth_plugin='mysql_native_password',
-            database ="Fitness")
-    
-    cursor = connection.cursor()
-
-    st.write("Connection made..\n")
 
     cursor.execute("SELECT first_name from user;")
     st.write(cursor.fetchall())
